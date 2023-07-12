8ed01d41224da789705ca24358ef78918c86077c
Rohit9252 <rohitbatra0165@gmail.com>
2023-07-12 18:58:02 +0530

    backend api completed

--- task-management-server/controllers/taskControllers.js
+++ task-management-server/controllers/taskControllers.js
@@ -39,19 +39,99 @@ exports.getTask = async (req, res) => {
   }
 };
 
-
 exports.postTask = async (req, res) => {
-    try {
-      const {title, description, dueDate  } = req.body;
-      console.log(dueDate)
-      if (!description) {
-        return res.status(400).json({ status: false, msg: "Description of task not found" });
-      }
-      const task = await Task.create({ user: req.user.id, description , dueDate, title});
-      res.status(200).json({ task, status: true, msg: "Task created successfully.." });
+  try {
+    const { title, description, dueDate } = req.body;
+    console.log(dueDate);
+    if (!description) {
+      return res
+        .status(400)
+        .json({ status: false, msg: "Description of task not found" });
     }
-    catch (err) {
-      console.error(err);
-      return res.status(500).json({ status: false, msg: "Internal Server Error" });
-    }
+    const task = await Task.create({
+      user: req.user.id,
+      description,
+      dueDate,
+      title,
+    });
+    res
+      .status(200)
+      .json({ task, status: true, msg: "Task created successfully.." });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ status: false, msg: "Internal Server Error" });
   }
+};
+
+exports.putTask = async (req, res) => {
+  try {
+    const { title, description, dueDate, completed } = req.body;
+    if (!description) {
+      return res
+        .status(400)
+        .json({ status: false, msg: "Description of task not found" });
+    }
+
+    if (!validateObjectId(req.params.taskId)) {
+      return res.status(400).json({ status: false, msg: "Task id not valid" });
+    }
+
+    let task = await Task.findById(req.params.taskId);
+    if (!task) {
+      return res
+        .status(400)
+        .json({ status: false, msg: "Task with given id not found" });
+    }
+
+    if (task.user != req.user.id) {
+      return res
+        .status(403)
+        .json({ status: false, msg: "You can't update task of another user" });
+    }
+
+    task = await Task.findByIdAndUpdate(
+      req.params.taskId,
+      { description, title, dueDate, completed },
+      { new: true }
+    );
+    res
+      .status(200)
+      .json({ task, status: true, msg: "Task updated successfully.." });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ status: false, msg: "Internal Server Error" });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    if (!validateObjectId(req.params.taskId)) {
+      return res.status(400).json({ status: false, msg: "Task id not valid" });
+    }
+
+    let task = await Task.findById(req.params.taskId);
+    if (!task) {
+      return res
+        .status(400)
+        .json({ status: false, msg: "Task with given id not found" });
+    }
+
+    if (task.user != req.user.id) {
+      return res
+        .status(403)
+        .json({ status: false, msg: "You can't delete task of another user" });
+    }
+
+    await Task.findByIdAndDelete(req.params.taskId);
+    res.status(200).json({ status: true, msg: "Task deleted successfully.." });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ status: false, msg: "Internal Server Error" });
+  }
+};
--- task-management-server/routes/taskroutes.js
+++ task-management-server/routes/taskroutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { verifyAccessToken } = require("../middleware/index");
-const { getTasks, getTask, postTask} = require("../controllers/taskControllers");
+const { getTasks, getTask, postTask, putTask, deleteTask } = require("../controllers/taskControllers");
 const validTokenHandler = require("../middleware/validTokenHandler");
 
 router.get("/", validTokenHandler, getTasks);
 router.get("/:taskId", validTokenHandler, getTask);
 router.post("/", validTokenHandler, postTask);
+router.put("/:taskId", validTokenHandler, putTask);
+router.delete("/:taskId", validTokenHandler, deleteTask);
+
 
 module.exports = router;
