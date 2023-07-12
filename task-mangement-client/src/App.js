cd0f5928c8119a301073736ba073be55335b2e75
Rohit9252 <rohitbatra0165@gmail.com>
2023-07-12 21:50:24 +0530

    home-page signup-page login-page created

--- task-mangement-client/src/App.js
+++ task-mangement-client/src/App.js
@@ -1,14 +1,40 @@
-
-import './App.css';
+import { useEffect } from "react";
 import Navbar from './components/Navbar';
-
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Task from "./pages/Task";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { saveProfile } from "./redux/actions/authActions";
+import NotFound from "./pages/NotFound";
 function App() {
+
+
+  const authState = useSelector(state => state.authReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+    dispatch(saveProfile(token));
+  }, [authState.isLoggedIn, dispatch]);
+
+
   console.log("App.js");
   return (
-    <div className="App">
-      <h1>App</h1>
-     <Navbar/>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={authState.isLoggedIn ? <Navigate to="/" /> : <Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/tasks/add" element={authState.isLoggedIn ? <Task /> : <Navigate to="/login" state={{ redirectUrl: "/tasks/add" }} />} />
+        <Route path="/tasks/:taskId" element={authState.isLoggedIn ? <Task /> : <Navigate to="/login" state={{ redirectUrl: window.location.pathname }} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </>
   );
 }
 
